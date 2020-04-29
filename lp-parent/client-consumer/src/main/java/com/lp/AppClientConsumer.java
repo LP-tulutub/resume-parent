3bb206ff5881111bfcd2a3ec2c5be836a7640b18
LP-tulutub <347863282@qq.com>
2020-04-29 10:02:46 +0800

    resume

--- lp-parent/client-consumer/src/main/java/com/lp/AppClientConsumer.java
+++ lp-parent/client-consumer/src/main/java/com/lp/AppClientConsumer.java
@@ -2,12 +2,14 @@ package com.lp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @EnableFeignClients
-//@EnableHystrix
+@EnableHystrix
 //@ServletComponentScan
 public class AppClientConsumer {
 

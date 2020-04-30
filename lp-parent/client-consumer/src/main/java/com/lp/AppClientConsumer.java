69cb78832c4ae2ed508771f79aa7f028f65ce17c
LP-tulutub <347863282@qq.com>
2020-04-30 16:42:56 +0800

    resume

--- lp-parent/client-consumer/src/main/java/com/lp/AppClientConsumer.java
+++ lp-parent/client-consumer/src/main/java/com/lp/AppClientConsumer.java
@@ -27,37 +27,35 @@ public class AppClientConsumer {
 
 
 	@Bean
-
 	public ServletWebServerFactory servletContainer() {
 
 		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-
-			@Override protected void postProcessContext(Context context) {
-
+			@Override
+			protected void postProcessContext(Context context) {
 				SecurityConstraint securityConstraint = new SecurityConstraint();
-
-				securityConstraint.setUserConstraint("CONFIDENTIAL"); SecurityCollection collection = new SecurityCollection(); collection.addPattern("/*");
-
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
 				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
 
-				context.addConstraint(securityConstraint); } };
-
-		tomcat.addAdditionalTomcatConnectors(redirectConnector()); return tomcat;
-
+		tomcat.addAdditionalTomcatConnectors(redirectConnector());
+		return tomcat;
 	}
 
-	private Connector redirectConnector() { Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL)
-
-			; connector.setScheme("http");
-
+	private Connector redirectConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
 		connector.setPort(80);
-
 		connector.setSecure(false);
 
 		connector.setRedirectPort(443);
-
 		return connector;
 
 	}
+
+
 
 }
